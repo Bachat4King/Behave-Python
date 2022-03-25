83f5dffc777fbf79f56af07d03d6c58fa7a66437
Bastian Alejandro Silva Fuenzalida <bastian.silva@globant.com>
2022-03-25 10:44:31 -0300

    [+] added disable-dev-shm-usage option

--- environment.py
+++ environment.py
@@ -11,6 +11,7 @@ def init_browser():
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         return webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
